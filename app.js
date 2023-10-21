a4606fd130015db3312864155deaf2b1e614f483
kara rah <ar.rahul715@gmail.com>
2023-10-21 09:57:24 +0000

    changes in javascript

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const accountHolderName = document.getElementById("account-holder-name");
+    const accountHolderNameDisplay = document.getElementById("displayed-account-holder-name");
     const accountType = document.getElementById("account-type");
     const balance = document.getElementById("balance");
     const action = document.getElementById("action");
@@ -8,9 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const accountForm = document.getElementById("account-form");
 
     let currentBalance = 0;
+    let accountHolderName = ""; // Initialize the accountHolderName variable
 
     // Function to update account information
     function updateAccountInfo() {
+        accountHolderNameDisplay.textContent = accountHolderName;
         accountType.textContent = "Savings"; // You can change this if needed
         balance.textContent = "$" + currentBalance.toFixed(2);
     }
@@ -38,11 +40,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 message.textContent = `Withdrawn $${amountValue.toFixed(2)}.`;
                 updateAccountInfo();
             }
-        }
 
-        // Clear form fields
-        action.selectedIndex = 0;
-        amount.value = "";
+            // Clear form fields
+            action.selectedIndex = 0;
+            amount.value = "";
+        }
+    });
+
+    // Function to handle name input
+    accountHolderNameDisplay.addEventListener("input", function () {
+        accountHolderName = accountHolderNameDisplay.textContent;
     });
 
     // Initialize account information
